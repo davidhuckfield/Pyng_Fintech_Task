3f0502b1c1fd9a8593bdb24619264e8ecda10444
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-07-20 14:52:31 +0100

    Basic login page structure and styling

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Comfortaa': require('./assets/fonts/Comfortaa-VariableFont_wght.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.logoContainer}>
+      <Image style={styles.logo} source={require('./assets/logo.png')} resizeMode='contain' />
+    </View>
+     
+     <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button1}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fbfbfb',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button1: {
+      backgroundColor: 'rgb(254,7,200)',
+      padding: 20,
+      borderRadius: 50,
+      margin: 10,
+      width:'100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  button2: {
+    backgroundColor: 'rgb(160,16,163)',
+    padding: 20,
+    borderRadius: 50,
+    margin: 10,
+    width:'100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontFamily: 'Comfortaa',
+  },
+  buttonsContainer: {
+    width: '90%',
+    padding: 20,
+    position:'absolute',
+    bottom: 30,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 200,
+    alignItems: 'center',
+    width:'70%',
+  },
+  logo: {
+    width: '100%',
   },
 });
