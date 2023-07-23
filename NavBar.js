bda2cbbf52000e6b5b8550ba6b921c1585f572a8
davidhuckfield <djhuckfield@hotmail.co.uk>
2023-07-23 18:35:15 +0100

    Added and positioned profile button

--- NavBar.js
+++ NavBar.js
@@ -17,11 +17,12 @@ export default function NavBar({ navigation }) {
   
     return (
       <View style={styles.container}>
+      <Image style={styles.profile} source={require('./assets/profileicon.png')} resizeMode='contain' />
       <View style={styles.navBar}>
         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('HomePage')}>
           <Image style={styles.image} source={require('./assets/group.png')} resizeMode='contain' />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('HomePage')}>
+        <TouchableOpacity style={styles.button2} onPress={() => navigation.navigate('HomePage')}>
           <Image style={styles.image} source={require('./assets/send.png')} resizeMode='contain' />
         </TouchableOpacity>
         <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('HomePage')}>
@@ -44,21 +45,35 @@ export default function NavBar({ navigation }) {
       justifyContent: 'center',
     },
     button: {
-        backgroundColor: 'rgb(254,7,200)',
-        padding: 10,
         borderRadius: 50,
         margin: 5,
-        width:'15%',
+        width:'23%',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    button2: {
+      backgroundColor: 'rgb(254,7,200)',
+      borderRadius: 50,
+      margin: 5,
+      width:'23%',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
     navBar: {
       backgroundColor: 'rgb(160,16,163)',
-      borderRadius: 25,
-      width: '90%',
+      borderRadius: 50,
+      width: '95%',
       padding: 0,
       position:'absolute',
-      bottom: 5,
+      bottom: 10,
+      flex:1,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    profile: {
+      position:'absolute',
+      top: 10,
+      right:10,
       flex:1,
       flexDirection: 'row',
       justifyContent: 'space-between',
